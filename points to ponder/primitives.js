fb44fbdeb81723f269d449f6984f1524721e99f0
rishij raj <rishij.raj@rapidinnovation.dev>
2022-08-05 18:07:51 +0530

    type - cohersion

--- points to ponder/primitives.js
+++ points to ponder/primitives.js
@@ -6,7 +6,13 @@
 // non-primitive : object 
 
 // primitive : immutable , does not contain any function to manupulate the value 
-// bt using autooxing the wrapper class arout the literal it can access the properties 
+// bt using autoxing the wrapper class arout the literal it can access the properties 
 
 let a = "test" ;  // internally : let a = new String("a") 
-let b = new String("test")  // 
+let b = new String("test")  //
+
+const ab = new Boolean(false) ;
+if({}) console.log("jhdajlsd") ;
+console.log(ab) ;
+console.log(ab == false)
+console.log(ab === false)       // type-cohersion
