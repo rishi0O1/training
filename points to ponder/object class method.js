176bf9df6fef6f7dde81f5c2b8084e8ead7938bd
rishij raj <rishij.raj@rapidinnovation.dev>
2022-08-05 17:26:05 +0530

    aa

--- points to ponder/object class method.js
+++ points to ponder/object class method.js
@@ -12,3 +12,31 @@ let x = Object.create(a) ;
 console.log(x) ;  // {} 
 console.log(x.a)  // rishij : by a object 
 
+// Object.values() :  taskes out all the values from the object
+const s = {
+    a: "rishij" ,
+}
+Object.values(s) ;  // [ "rishij" ]
+
+// Object.defineProperty()
+// By default, values added using Object.defineProperty() are immutable and not enumerable.
+const object1 = {} 
+Object.defineProperty(object1 , "test" , {
+    value: 123 ,
+})
+console.log(object1) ;
+console.log(" test -> s  " , s) ;
+console.log(Object.values(object1)) ;
+
+
+// Object.entries() 
+const object3 = {
+    "name": "rishij" ,
+    "nam": "rishij" ,
+}
+console.log(" object3  " , object3);
+console.log(Object.entries(object3))
+
+// OBJECT.freeze() 
+const z = Object.freeze(object3) ; z.name = "rishijd" ;
+console.log(z)
